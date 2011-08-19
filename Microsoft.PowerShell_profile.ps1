feacc2631d14fda30981526753c8a72f3285cdf0
Douglas Mendizabal <dougmendizabal@gmail.com>
2011-08-18 19:57:43 -0500

    Added PowerShell profile.

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+# Set default location to Workspaces
+set-location C:\Users\dmendizabal\Workspaces
+
+# Aliases
+set-alias which Get-Command
+set-alias npp "C:\Program Files (x86)\Notepad++\notepad++.exe"
+set-alias vi "C:\Program Files (x86)\Vim\vim73\vim.exe"
